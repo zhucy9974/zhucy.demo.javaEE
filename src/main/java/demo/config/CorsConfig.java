1f0b4b4e9d63223fa419999cfaf66a15b566a8aa
zhucy9974 <75172940+zhucy9974@users.noreply.github.com>
2026-08-19 00:13:21 +0200

    chore: allow new Firebase domain in CORS

--- src/main/java/demo/config/CorsConfig.java
+++ src/main/java/demo/config/CorsConfig.java
@@ -12,7 +12,9 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins(
                         "http://localhost:4200",
                         "https://zhucy-demo-angular.web.app",
-                        "https://zhucy-demo-angular.firebaseapp.com")// 1 设置访问源地址
+                        "https://zhucy-demo-angular.firebaseapp.com",
+                        "https://chongyang-studio.web.app",
+                        "https://chongyang-studio.firebaseapp.com")// 1 设置访问源地址
                 .allowCredentials(true)// 2 设置访问源请求头
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 3 设置访问源请求方法
                 .maxAge(3600);
